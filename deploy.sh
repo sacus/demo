7c99d2c8999f895896dd914191816e0756389356
clc <clc@yarnway.com>
2016-03-30 22:54:54 +0800

    2th commit

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+################################################
+# this script is created by clc.
+# e_mail:474335602@qq.com
+# qqinfo:474335602	
+# function:
+# version:1.0
+################################################
+echo hehe
